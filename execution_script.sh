d9bba8e36b37718743d0f5c92ee5bbfe8e96bcad
0x5900FX <169356813+0x5900FX@users.noreply.github.com>
2025-07-08 07:37:41 +0100

    execution script update

--- execution_script.sh
+++ execution_script.sh
@@ -3,6 +3,8 @@
 filename="${1%%.*}"
 nasm -f elf64 ${filename}".s"
 ld -o ${filename} ${filename}".o"
+mv ${filename}.s  source_file/
+mv ${filename}.o  object_file/
 [ "$2" == "-g" ] && gdb - q ${filename} || ./${filename}
 
 
